54b958bc9515a076c61849c2ecd0c7e24a823bf9
Jacob Vega/Canote <jacob@webdogs.com>
2015-03-03 06:19:09 -0800

    Initial
    My first commit

---
+++ csv-product-report-manager/prm-support-form.php
@@ -0,0 +1,16 @@
+<table style="font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;text-size-adjust:100%;-webkit-text-size-adjust:100%;height:100%;background:#FFFFFF;margin:-11px -12px 6px;min-width:100%;width:auto;position:relative;" border="0" width="100%" height="100%" cellspacing="0" cellpadding="4">
+    <tbody>
+        <tr>
+            <td width="1%">&nbsp;</td>
+            <td style="width: 98%;text-size-adjust:100%;-webkit-text-size-adjust:100%;" width="98%" valign="top" colspan="2">
+                <div id="prm_form_wrapper">
+                    <?php
+                    echo $current_files;
+                    ?>
+                </div>
+                <input id='prm_form_reset' type='button' class='button' value='Run Report' />
+            </td>
+            <td width="1%">&nbsp;</td>
+        </tr>
+    </tbody>
+</table>
